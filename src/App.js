8936e857946a888f05d6c5208433977384569361
petriken <petriken82@gmail.com>
2019-03-04 21:33:22 +0300

    fix: fix no link on PR even with score

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
 // import React, { Component } from 'react';
-import './App.scss';
-import React from 'react';
-import Select from 'react-select';
-import data from './data.json';
+import "./App.scss";
+import React from "react";
+import Select from "react-select";
+import data from "./data.json";
 
 const mentorsList = [];
 const dataFile = Object.keys(data.mentors);
-let currentMentor = '';
+let currentMentor = "";
 
 // eslint-disable-next-line
 for (let i = 0; i < dataFile.length; i++) {
@@ -31,20 +31,20 @@ function getTaskName(name) {
 function getStatistics(name) {
   const commonCountTask = Object.entries(data)[2][1];
   const countCurrentTask = Object.values(taskName[name]);
-  const percent = countCurrentTask[3] / commonCountTask * 100;
+  const percent = (countCurrentTask[3] / commonCountTask) * 100;
   if (percent > 0) {
-    return Math.round(percent)+' %';
+    return Math.round(percent) + " %";
   }
 }
 
 function getTaskStatus(name) {
   const getStatus = Object.values(taskName[name]);
-  return getStatus[1].replace(/\s+/g, '');
+  return getStatus[1].replace(/\s+/g, "");
 }
 
 function getStudenName(studentName, mentor) {
   const getStudentUrl = Object.values(
-    data.mentors[mentor].mentorStudents[studentName],
+    data.mentors[mentor].mentorStudents[studentName]
   );
   const studentGithubUrl = Object.entries(getStudentUrl)[1][1];
   return studentGithubUrl;
@@ -52,33 +52,33 @@ function getStudenName(studentName, mentor) {
 
 function getScore(studentName, mentor, currentTaskName) {
   const getStudentUrl = Object.values(
-    data.mentors[mentor].mentorStudents[studentName],
+    data.mentors[mentor].mentorStudents[studentName]
   );
   const score = Object.entries(getStudentUrl)[3][1][currentTaskName];
-    if (score) {
-     return score;
-  } 
+  if (score) {
+    return score;
+  }
   // return score;
 }
 
 function getPrTask(studentName, mentor, currentTaskName) {
   const getStudentUrl = Object.values(
-    data.mentors[mentor].mentorStudents[studentName],
+    data.mentors[mentor].mentorStudents[studentName]
   );
   const pr = Object.entries(getStudentUrl)[4][1][currentTaskName];
   const score = Object.entries(getStudentUrl)[3][1][currentTaskName];
-  if (getTaskStatus(currentTaskName) === 'Checking') {
-    return '#';
+  if (getTaskStatus(currentTaskName) === "Checking" && (score <= 0 || !score)) {
+    return "#";
   }
   if (!score || score === 0) {
-    return ;
+    return;
   }
   return pr;
 }
 
 function getCurrentMentor(mentor) {
-  if (localStorage.getItem('currentMentor')) {
-    currentMentor = localStorage.getItem('currentMentor');
+  if (localStorage.getItem("currentMentor")) {
+    currentMentor = localStorage.getItem("currentMentor");
   }
   if (mentor) {
     currentMentor = mentor.value;
@@ -86,10 +86,10 @@ function getCurrentMentor(mentor) {
   return currentMentor;
 }
 
-const getStudent = (mentor) => {
+const getStudent = mentor => {
   let students = [];
-  if (localStorage.getItem('currentMentor')) {
-    currentMentor = localStorage.getItem('currentMentor');
+  if (localStorage.getItem("currentMentor")) {
+    currentMentor = localStorage.getItem("currentMentor");
     students = Object.keys(data.mentors[currentMentor].mentorStudents);
   }
   if (mentor) {
@@ -103,7 +103,9 @@ function setStudent(mentor) {
   return getStudent(mentor).map(studentName => (
     <td className="studentName cell" key={studentName}>
       <a
-        className="link" rel="noopener noreferrer" target="_blank"
+        className="link"
+        rel="noopener noreferrer"
+        target="_blank"
         href={getStudenName(studentName, getCurrentMentor(mentor))}
       >
         {studentName}
@@ -114,10 +116,24 @@ function setStudent(mentor) {
 
 function setScore(mentor, name) {
   return getStudent(mentor).map(studentName => (
-    <td style={{ textAlign: 'center' }} className={!getScore(studentName, getCurrentMentor(mentor), name) && getTaskStatus(name) === 'Checked' ? 'failed' : getTaskStatus(name)} key={studentName}><a
-      className="link" rel="noopener noreferrer" target="_blank" href={getPrTask(studentName, getCurrentMentor(mentor), name)}
-    >{getScore(studentName, getCurrentMentor(mentor), name)} 
-    </a>
+    <td
+      style={{ textAlign: "center" }}
+      className={
+        !getScore(studentName, getCurrentMentor(mentor), name) &&
+        getTaskStatus(name) === "Checked"
+          ? "failed"
+          : getTaskStatus(name)
+      }
+      key={studentName}
+    >
+      <a
+        className="link"
+        rel="noopener noreferrer"
+        target="_blank"
+        href={getPrTask(studentName, getCurrentMentor(mentor), name)}
+      >
+        {getScore(studentName, getCurrentMentor(mentor), name)}
+      </a>
     </td>
   ));
 }
@@ -126,11 +142,16 @@ function setTask(mentor) {
   return task.map(name => (
     <tr key={name}>
       <td className={getTaskStatus(name)}>
-        <a className="link taskname" rel="noopener noreferrer" target="_blank" href={getTaskName(name)}>
+        <a
+          className="link taskname"
+          rel="noopener noreferrer"
+          target="_blank"
+          href={getTaskName(name)}
+        >
           {name}
         </a>
       </td>
-      <td className={getTaskStatus(name)} style={{ textAlign: 'center' }}>
+      <td className={getTaskStatus(name)} style={{ textAlign: "center" }}>
         {getStatistics(name)}
       </td>
       {setScore(mentor, name)}
@@ -140,19 +161,18 @@ function setTask(mentor) {
 
 class App extends React.Component {
   state = {
-    selectedOption: null,
+    selectedOption: null
   };
 
-  handleChange = (selectedOption) => {
+  handleChange = selectedOption => {
     this.setState({ selectedOption });
-    localStorage.setItem('currentMentor', selectedOption.value);
+    localStorage.setItem("currentMentor", selectedOption.value);
   };
 
   render() {
     const { selectedOption } = this.state;
 
     return (
-      
       <div className="App">
         <Select
           value={selectedOption}
@@ -168,50 +188,44 @@ class App extends React.Component {
             </tr>
           </thead>
         </table>
-<div className="wrapper">
-        <table align="center" className="table task-table">
-          <thead>
-            <tr>
-              <td />
-              <td className="statistics cell">
-                statistics
-              </td>
-              {setStudent(selectedOption)}
-            </tr>
-          </thead>
-          <tbody>
-            {setTask(selectedOption)}
-          </tbody>
-        </table>
+        <div className="wrapper">
+          <table align="center" className="table task-table">
+            <thead>
+              <tr>
+                <td />
+                <td className="statistics cell">statistics</td>
+                {setStudent(selectedOption)}
+              </tr>
+            </thead>
+            <tbody>{setTask(selectedOption)}</tbody>
+          </table>
         </div>
-      
+
         <table className="table description-table">
           <tbody className="tbody">
             <tr>
-              <td className="Checked description-table__cell"></td>
+              <td className="Checked description-table__cell" />
               <td className="description-table__cell">Checked by mentor</td>
             </tr>
             <tr>
-              <td className="Checking description-table__cell"></td>
+              <td className="Checking description-table__cell" />
               <td className="description-table__cell">Checking by mentor</td>
             </tr>
             <tr>
-              <td className="InProgress description-table__cell"></td>
+              <td className="InProgress description-table__cell" />
               <td className="description-table__cell">In Progress</td>
             </tr>
             <tr>
-              <td className="ToDo description-table__cell"></td>
+              <td className="ToDo description-table__cell" />
               <td className="description-table__cell">ToDo</td>
             </tr>
             <tr>
-              <td className="failed description-table__cell"></td>
+              <td className="failed description-table__cell" />
               <td className="description-table__cell">No solution</td>
             </tr>
           </tbody>
         </table>
-
       </div>
-      
     );
   }
 }
