7c34c5b8c7f774a4f7343acfe190047373a93bab
petriken <petriken82@gmail.com>
2019-03-20 23:14:15 +0300

    feat: add time to check tasks

---
+++ src/App.js
@@ -0,0 +1,279 @@
+/* eslint-disable no-console */
+// import React, { Component } from 'react';
+import "./App.scss";
+import React from "react";
+import Select from "react-select";
+import data from "./data.json";
+
+const mentorsList = [];
+const dataFile = Object.keys(data.mentors);
+let currentMentor = "";
+
+// eslint-disable-next-line
+for (let i = 0; i < dataFile.length; i++) {
+  const mentorLogin = dataFile[i];
+  const mentorObject = {};
+
+  mentorObject.value = mentorLogin;
+  mentorObject.label = mentorLogin;
+  mentorsList.push(mentorObject);
+}
+
+const task = Object.keys(data.tasksStatus);
+
+const taskName = Object.entries(data)[1][1];
+
+function getTaskName(name) {
+  const getUrl = Object.values(taskName[name]);
+  return getUrl[2];
+}
+function getCheckTaskTime(name) {
+  const CheckTaskTime = Object.values(taskName[name])[4];
+  return CheckTaskTime;
+}
+function getStatistics(name) {
+  const commonCountTask = Object.entries(data)[2][1];
+  const countCurrentTask = Object.values(taskName[name]);
+  const percent = (countCurrentTask[3] / commonCountTask) * 100;
+  if (percent > 0) {
+    return Math.round(percent) + " %";
+  }
+}
+
+function getTaskStatus(name) {
+  const getStatus = Object.values(taskName[name]);
+  return getStatus[1].replace(/\s+/g, "");
+}
+
+function getStudenName(studentName, mentor) {
+  const getStudentUrl = Object.values(
+    data.mentors[mentor].mentorStudents[studentName]
+  );
+  const studentGithubUrl = Object.entries(getStudentUrl)[1][1];
+  return studentGithubUrl;
+}
+
+function getScore(studentName, mentor, currentTaskName) {
+  const getStudentUrl = Object.values(
+    data.mentors[mentor].mentorStudents[studentName]
+  );
+  const score = Object.entries(getStudentUrl)[3][1][currentTaskName];
+  if (score) {
+    return score;
+  }
+}
+
+function getStudentStatus(mentor, studentName) {
+  const studentsStatus = Object.values(
+    data.mentors[mentor].mentorStudents[studentName]
+  )[5];
+  if (studentsStatus === "dismissed") {
+    return studentsStatus + " studentName cell";
+  }
+  return "studentName cell";
+}
+
+function getPrTask(studentName, mentor, currentTaskName) {
+  const getStudentUrl = Object.values(
+    data.mentors[mentor].mentorStudents[studentName]
+  );
+  const pr = Object.entries(getStudentUrl)[4][1][currentTaskName];
+  const score = Object.entries(getStudentUrl)[3][1][currentTaskName];
+  if (getTaskStatus(currentTaskName) === "Checking" && (score <= 0 || !score)) {
+    return "#";
+  }
+  if (!score || score === 0) {
+    return;
+  }
+  return pr;
+}
+
+function getCurrentMentor(mentor) {
+  if (localStorage.getItem("currentMentor")) {
+    currentMentor = localStorage.getItem("currentMentor");
+  }
+  if (mentor) {
+    currentMentor = mentor.value;
+  }
+  return currentMentor;
+}
+
+const getStudent = mentor => {
+  let students = [];
+  if (localStorage.getItem("currentMentor")) {
+    currentMentor = localStorage.getItem("currentMentor");
+    students = Object.keys(data.mentors[currentMentor].mentorStudents);
+  }
+  if (mentor) {
+    currentMentor = mentor.value;
+    students = Object.keys(data.mentors[currentMentor].mentorStudents);
+  }
+  return students;
+};
+
+function setClass(studentName, mentor, name) {
+  const studentsStatus = Object.values(
+    data.mentors[getCurrentMentor(mentor)].mentorStudents[studentName]
+  )[5];
+  if (
+    !getScore(studentName, getCurrentMentor(mentor), name) &&
+    getTaskStatus(name) === "Checked" &&
+    studentsStatus !== "dismissed"
+  ) {
+    return "failed";
+  } else if (
+    !getScore(studentName, getCurrentMentor(mentor), name) &&
+    getTaskStatus(name) === "Checked" &&
+    studentsStatus === "dismissed"
+  ) {
+    return "failed dismissed";
+  } else if (
+    getTaskStatus(name) &&
+    studentsStatus === "dismissed"
+  ) {
+    return getTaskStatus(name) + " dismissed";
+  } else {
+    return getTaskStatus(name);
+  }
+}
+
+function setStudent(mentor) {
+  return getStudent(mentor).map(studentName => (
+    <td
+      className={getStudentStatus(getCurrentMentor(mentor), studentName)}
+      key={studentName}
+    >
+      <a
+        className="link"
+        rel="noopener noreferrer"
+        target="_blank"
+        href={getStudenName(studentName, getCurrentMentor(mentor))}
+      >
+        {studentName}
+      </a>
+    </td>
+  ));
+}
+
+function setScore(mentor, name) {
+  return getStudent(mentor).map(studentName => (
+    <td
+      style={{ textAlign: "center" }}
+      className={setClass(studentName, mentor, name)}
+      key={studentName}
+    >
+      <a
+        className="link"
+        rel="noopener noreferrer"
+        target="_blank"
+        href={getPrTask(studentName, getCurrentMentor(mentor), name)}
+      >
+        {getScore(studentName, getCurrentMentor(mentor), name)}
+      </a>
+    </td>
+  ));
+}
+
+function setTask(mentor) {
+  return task.map(name => (
+    <tr key={name}>
+      <td className={getTaskStatus(name)}>
+        <a
+          className="link taskname"
+          rel="noopener noreferrer"
+          target="_blank"
+          href={getTaskName(name)}
+        >
+          {name}
+        </a>
+      </td>
+      <td className={getTaskStatus(name)} style={{ textAlign: "center" }}>
+      {getCheckTaskTime(name)}
+      </td>
+      <td className={getTaskStatus(name)} style={{ textAlign: "center" }}>
+        {getStatistics(name)}
+      </td>
+      {setScore(mentor, name)}
+    </tr>
+  ));
+}
+
+class App extends React.Component {
+  state = {
+    selectedOption: null
+  };
+
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    localStorage.setItem("currentMentor", selectedOption.value);
+  };
+
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <div className="App">
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={mentorsList}
+        />
+
+        <table className="table mentor-table">
+          <thead className="thead">
+            <tr>
+              <td className="mentorTitle">Ментор</td>
+              <td className="mentorName">{getCurrentMentor(selectedOption)}</td>
+            </tr>
+          </thead>
+        </table>
+        <div className="wrapper">
+          <table align="center" className="table task-table">
+            <thead>
+              <tr>
+                <td />
+                <td className="check_time cell ">check&nbsp;time</td>
+                <td className="statistics cell">statistics</td>
+                {setStudent(selectedOption)}
+              </tr>
+            </thead>
+            <tbody>{setTask(selectedOption)}</tbody>
+          </table>
+        </div>
+
+        <table className="table description-table">
+          <tbody className="tbody">
+            <tr>
+              <td className="Checked description-table__cell" />
+              <td className="description-table__cell">Checked by mentor</td>
+            </tr>
+            <tr>
+              <td className="Checking description-table__cell" />
+              <td className="description-table__cell">Checking by mentor</td>
+            </tr>
+            <tr>
+              <td className="InProgress description-table__cell" />
+              <td className="description-table__cell">In Progress</td>
+            </tr>
+            <tr>
+              <td className="ToDo description-table__cell" />
+              <td className="description-table__cell">ToDo</td>
+            </tr>
+            <tr>
+              <td className="failed description-table__cell" />
+              <td className="description-table__cell">No solution</td>
+            </tr>
+            <tr>
+              <td className="dismissed description-table__cell">
+                semitransparent student -{" "}
+              </td>
+              <td className="description-table__cell"> student dismissed</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/indexScript.js
@@ -0,0 +1,239 @@
+/* eslint-disable no-console */
+const fs = require("fs");
+
+// eslint-disable-next-line
+if (typeof require !== "undefined") XLSX = require("xlsx");
+
+// eslint-disable-next-line
+const studentMentorPair = XLSX.readFile(
+  "./src/data/Mentor-students pairs.xlsx"
+);
+const sheetPairs = studentMentorPair.Sheets.pairs;
+const sheetMentors = studentMentorPair.Sheets["second_name-to_github_account"];
+
+// eslint-disable-next-line
+const studentList = XLSX.readFile("./src/data/Score RSSchool 2018Q3 (Responses) 2019-03-11.xlsx");
+const sheetScore = studentList.Sheets["Form Responses 1"];
+
+// eslint-disable-next-line
+const taskStatus = XLSX.readFile("./src/data/Tasks.xlsx");
+const sheetStatus = taskStatus.Sheets.Sheet1;
+
+const mentorsList = new Map();
+const studentsList = new Map();
+const mentorsResult = {};
+const statusResult = {};
+const finalStructure = {};
+let dismissedStudents = [];
+let countStudent = 0;
+const tasksCount = {};
+
+let taskCount = 0;
+
+const fieldMappingPairs = {
+  mentorName: "A",
+  studentName: "B"
+};
+
+const fieldMappingMentor = {
+  name: "A",
+  Surname: "B",
+  Github: "E"
+};
+
+const fieldMappingScore = {
+  mentorGithub: "B",
+  studentGithub: "C",
+  task: "D",
+  score: "F",
+  pr: "E",
+  action: "H",
+};
+
+const fieldMappingStatus = {
+  taskName: "A",
+  taskStatus: "C",
+  taskLink: "B",
+  checkTaskTime: "D"
+};
+
+// eslint-disable-next-line
+const loginPattern = /\/([^\/]+)\/?$/;
+
+const getMentor = (sheet, currentRow) => {
+  const mentorData = {
+    name: sheet[fieldMappingMentor.name + currentRow].v,
+    Surname: sheet[fieldMappingMentor.Surname + currentRow].v,
+    ID: `${sheet[fieldMappingMentor.name + currentRow].v} ${
+      sheet[fieldMappingMentor.Surname + currentRow].v
+      }`,
+    Github: sheet[fieldMappingMentor.Github + currentRow].v,
+    GithubLogin: sheet[fieldMappingMentor.Github + currentRow].v.split(
+      loginPattern
+    )[1],
+    mentorStudents: {}
+  };
+  return mentorData;
+};
+
+const getMentors = sheet => {
+  let count = 2;
+  while (sheet[fieldMappingMentor.Github + count]) {
+    const mentor = getMentor(sheet, count);
+
+    mentorsList.set(mentor.ID, mentor);
+    // eslint-disable-next-line
+    count++;
+  }
+};
+
+const getScore = (sheet, currentRow) => {
+
+  let studentData = {
+    mentorGithub: sheet[fieldMappingScore.mentorGithub + currentRow].v,
+    studentGithub: sheet[fieldMappingScore.studentGithub + currentRow].v,
+    studentName: sheet[fieldMappingScore.studentGithub + currentRow].v
+      .split(loginPattern)[1]
+      ? sheet[fieldMappingScore.studentGithub + currentRow].v
+        .split(loginPattern)[1]
+        .toLowerCase()
+      : sheet[fieldMappingScore.studentGithub + currentRow].v
+        .split(loginPattern)[0],
+    tasks: sheet[fieldMappingScore.task + currentRow]
+      ? {
+        [sheet[fieldMappingScore.task + currentRow].v]:
+          sheet[fieldMappingScore.score + currentRow].v
+      }
+      : { "task failed": "task failed" },
+    prLinks: sheet[fieldMappingScore.task + currentRow]
+      ? {
+        [sheet[fieldMappingScore.task + currentRow].v]:
+          sheet[fieldMappingScore.pr + currentRow].v
+      }
+      : { "task failed": "task failed" },
+    studentStatus: '',
+  };
+  if
+    (sheet[fieldMappingScore.action + currentRow] && sheet[fieldMappingScore.action + currentRow].v === "Отчислить студента") { dismissedStudents.push(studentData.studentName) };
+  return studentData;
+};
+
+const getTasks = sheet => {
+  let count = 2;
+  while (sheet[fieldMappingScore.mentorGithub + count]) {
+    const student = getScore(sheet, count);
+
+    if (!studentsList.get(student.studentName)) {
+      studentsList.set(student.studentName, student);
+      countStudent++
+    } else {
+      const key = Object.keys(student.tasks)[0];
+      studentsList.get(student.studentName).tasks[key] = student.tasks[key];
+      const keyPr = Object.keys(student.prLinks)[0];
+      studentsList.get(student.studentName).prLinks[keyPr] =
+        student.prLinks[key];
+    }
+    if (dismissedStudents.indexOf(studentsList.get(student.studentName).studentName) !== -1) { studentsList.get(student.studentName).studentStatus = 'dismissed' }
+    else { studentsList.get(student.studentName).studentStatus = 'active' };
+    // eslint-disable-next-line
+    count++;
+  }
+};
+
+const getCountTask = (sheet, nameTask) => {
+  let count = 2;
+  let countTask = 0;
+  while (sheet[fieldMappingScore.mentorGithub + count]) {
+    if (sheet[fieldMappingScore.task + count] && sheet[fieldMappingScore.task + count].v === nameTask) {
+      countTask++;
+    }
+    // else {
+    //   continue
+    // }
+    // eslint-disable-next-line
+    count++;
+  }
+  return countTask;
+};
+
+const getStatusTask = (sheet, currentRow, sheetStudent) => {
+  const taskData = {
+    taskName: sheet[fieldMappingStatus.taskName + currentRow].v.trim(),
+    taskStatus: sheet[fieldMappingStatus.taskStatus + currentRow].v,
+    taskLink: sheet[fieldMappingStatus.taskLink + currentRow]
+      ? sheet[fieldMappingStatus.taskLink + currentRow].v
+      : "no link",
+    taskCount: getCountTask(
+      sheetStudent,
+      sheet[fieldMappingStatus.taskName + currentRow].v.trim(),
+    ),
+    checkTaskTime: sheet[fieldMappingStatus.checkTaskTime + currentRow] ? sheet[fieldMappingStatus.checkTaskTime + currentRow].v : '',
+  };
+  return taskData;
+};
+
+const getStatus = (sheet, sheetStudent) => {
+  let count = 2;
+  while (sheet[fieldMappingStatus.taskName + count]) {
+    const status = getStatusTask(sheet, count, sheetStudent);
+
+    statusResult[status.taskName] = status;
+    // eslint-disable-next-line
+    count++;
+  }
+};
+
+const getPair = (sheet, currentRow) => {
+  const pair = {
+    mentorName: sheet[fieldMappingPairs.mentorName + currentRow].v,
+    studentName: sheet[fieldMappingPairs.studentName + currentRow].v
+  };
+
+  return pair;
+};
+
+const getPairs = sheet => {
+  let count = 2;
+  while (sheet[fieldMappingPairs.mentorName + count]) {
+    const pair = getPair(sheet, count);
+    const mentor = mentorsList.get(pair.mentorName);
+    const student = studentsList.get(pair.studentName);
+
+    if (student) {
+      mentor.mentorStudents[student.studentName] = student;
+      taskCount++;
+      // } else {
+      //   mentor.mentorStudents[pair.studentName] = {
+      //     studentName: pair.studentName,
+      //     tasks: {},
+      //   };
+    }
+
+    mentorsResult[mentor.GithubLogin] = mentor;
+    // eslint-disable-next-line
+    count++;
+  }
+};
+
+getMentors(sheetMentors);
+getTasks(sheetScore);
+getStatus(sheetStatus, sheetScore);
+getPairs(sheetPairs);
+
+// console.log('countStudent', countStudent);
+// console.log('dismissedStudents', dismissedStudents);
+// console.log('lenght', dismissedStudents.length);
+
+// FINAL RESULT **********************
+
+finalStructure.mentors = mentorsResult;
+finalStructure.tasksStatus = statusResult;
+finalStructure.taskCount = taskCount;
+
+// +++++++++++++++++++++++++++++++++
+
+const json = JSON.stringify(finalStructure, 0, 2);
+
+fs.writeFile("./src/data.json", json, "utf8", () => {
+  console.log("JSON is done!");
+});
