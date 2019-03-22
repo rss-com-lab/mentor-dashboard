0d5a5bfb5da1d0f6f61a76bac0adcdcb35e67d69
Paval Miatlitski <paval.escoba@gmail.com>
2019-03-22 14:31:20 +0300

    docs: add draft for further changes

---
+++ src/components/code-for-routing.js
@@ -0,0 +1,33 @@
+// index.js
+
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import App from "../App";
+
+// Here we import our components as soon as they will be ready
+
+// import Dashboard from './components/Dashboard/Dashboard';
+// import Form from './components/Form/Form';
+
+// And here we're making routing between components
+// ReactDOM.render((
+//   <App>
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path='/' component={Dashboard} />
+//         <Route path='./form' component={Form} />
+//       </Switch>
+//     </BrowserRouter>
+//   </App>
+// ))
+
+// App.js
+
+// All code above should be put into a component, if it's possible.
+// And the App component should look smth like this:
+// const App = ({ children }) => {
+//   <Fragment>
+//     <Header />
+//     {children}
+//     <Footer />
+//   </Fragment>
+// }
