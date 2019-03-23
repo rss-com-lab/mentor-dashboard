1fd076f3f3bbb872345424d4777175f408a363e8
petriken <petriken82@gmail.com>
2019-03-23 03:36:27 +0300

    feat: add srtucture and components to project

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,1 +1,23 @@
-// here goes Form component
+import "./Form.scss";
+import React from "react";
+import Select from "react-select";
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Select />
+
+        <table className="table mentor-table">
+          <thead className="thead">
+            <tr>
+              <td className="mentorTitle">Заглушка</td>
+              <td className="mentorTitle">формы</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Form;
