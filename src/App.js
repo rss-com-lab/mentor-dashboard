0b5599eb53c0a940ee59006f60f11cd177e64dd1
uniorunr <gagael23@live.com>
2019-03-27 23:59:25 +0300

    fix: fix eslint errors in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-console */
-import React from // ,{ Fragment }
-  "react";
-import "./App.scss";
+import React from /* ,{ Fragment } */ 'react';
 // import Footer from "./components/Footer/Footer";
 // import Header from "./components/Header/Header";
 
@@ -13,20 +11,24 @@ import {
   BrowserRouter as Router,
   // Link,
   Route,
-  Switch
-} from "react-router-dom";
+  Switch,
+} from 'react-router-dom';
 
-import Dashboard from "./components/Dashboard/Dashboard";
-import Form from "./components/Form/Form";
+import Dashboard from './components/Dashboard/Dashboard';
+import Form from './components/Form/Form';
+import './App.scss';
 
-// We give each route either a target `component`, or we can send functions in `render` or `children`
-// that return valid nodes. `children` always returns the given node whether there is a match or not.
+/* We give each route either a target `component`,
+or we can send functions in `render` or `children` */
+
+/* that return valid nodes.
+`children` always returns the given node whether there is a match or not. */
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL + '/'}>
+  <Router basename={`${process.env.PUBLIC_URL}/`}>
     <div>
       {/* <Fragment>
         <Header /> */}
-      {/* <Link to="/">Dashboard</Link>{' '}*/}
+      {/* <Link to="/">Dashboard</Link>{' '} */}
       {/* <Link to={{pathname: '/form'}}>Form</Link>{' '} */}
       <Switch>
         <Route exact path ="/" component={Dashboard} />
