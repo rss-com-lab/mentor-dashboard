facb67023e08f8e2a2de7356fb60479915a2e7e3
uniorunr <gagael23@live.com>
2019-04-11 22:45:21 +0300

    feature: move styles of Login component to separate file

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import FireBase from '../../../firebase/firebase';
+import './Login.scss';
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit * 2,
-    marginRight: 0,
-  },
+const styles = () => ({
   input: {
     display: 'none',
   },
@@ -17,23 +13,18 @@ const styles = theme => ({
 class LoginButton extends Component {
   handleClick = async () => {
     await FireBase.auth();
-  }
+  };
 
   render() {
-    const { classes } = this.props;
     return (
       <Button
         variant="outlined"
-        className={classes.button}
+        className="login-button"
         onClick={this.handleClick}
       >Login
       </Button>
     );
   }
 }
-
-LoginButton.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired,
-};
 
 export default withStyles(styles)(LoginButton);
