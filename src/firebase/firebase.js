a829e59df3f275134d001c640dcde7218a9a75f9
uniorunr <gagael23@live.com>
2019-04-11 17:48:10 +0300

    feature: add 'auth' and 'logout' static methods to Firebase class

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 const API_KEY = 'AIzaSyCX-HtE8YASWxl9k6DpQblWdLOvzsEgqUs';
 const AUTH_DOMAIN = 'rss-mentor-dashboard-2185d.firebaseapp.com';
@@ -18,6 +19,37 @@ class FireBase {
       messagingSenderId: MESSAGING_SENDER_ID,
     };
     firebase.initializeApp(config);
+  }
+
+  static async auth() {
+    const token = await firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider())
+      .then(result => result.credential.accessToken)
+      .catch((error) => {
+        throw new Error(error);
+      });
+
+    const userData = await fetch(`https://api.github.com/user?access_token=${token}`)
+      .then(response => response.json())
+      .catch((error) => {
+        throw new Error(error);
+      });
+
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: userData.login,
+      photoURL: userData.avatar_url,
+    }).catch((error) => {
+      throw new Error(error);
+    });
+  }
+
+  static logout() {
+    firebase.auth().signOut().then(() => {
+      window.location.reload();
+    }).catch((error) => {
+      throw new Error(error);
+    });
   }
 }
 
