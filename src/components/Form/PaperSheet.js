66566bf14ab1bfbb4f2ca2242bcc2a3952e795f6
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-04-17 10:55:56 +0300

    style: center form on the page

--- src/components/Form/PaperSheet.js
+++ src/components/Form/PaperSheet.js
@@ -10,13 +10,25 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
   },
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginTop: 20,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
 });
 
 function PaperSheet(props) {
   const { classes, children } = props;
 
   return (
-    <div>
+    <div className={classes.main}>
       <Paper className={classes.root} elevation={1}>
         {children}
       </Paper>
